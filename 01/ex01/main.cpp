b1aafd0b0f982e9d5cd0a22dd8e89e86b4e904d4
pvcordeiro <pv.35@hotmail.com>
2025-07-06 11:23:53 +0100

    fixed edge case in ex01

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -5,6 +5,9 @@ int	main(void)
 	int		N = 5;
 	Zombie *zombies = zombieHorde(N, "joselito");
 
+	if (!zombies)
+		return (std::cout << "Error: Invalid number of zombies" << std::endl, 1);
+
 	for (int i = 0; i < N; ++i)
 		zombies[i].announce();
 	std::cout << std::endl;
